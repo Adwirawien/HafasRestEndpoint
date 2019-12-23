6f4349e48bae3f26da46d17c1a72fb2f4332e684
Adwirawien <hannsadrian@gmail.com>
2019-12-23 17:52:16 +0100

    Fix issues

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const createHafas = require("hafas-client");
 const bvgProfile = require("hafas-client/p/bvg");
 
-const createApi = require(".");
+const createApi = require("hafas-rest-api");
 
 const config = {
   hostname: "api.transportflow.online",
   port: 3000,
   name: "hafas-rest-endpoint",
   homepage: "https://github.com/Adwirawien/HafasRestEndpoint",
-  version: "1.0.0"
+  version: "1.0.0",
+  description: "Transportflow hosted hafas-rest-api",
+  docsLink: "-"
 };
 
 const hafas = createHafas(bvgProfile, "hafas-rest-endpoint");
-const api = createApi(hafas, config);
+const api = createApi(hafas, config, () => {});
 
 api.listen(config.port, err => {
   if (err) console.error(err);

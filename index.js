4752334d1c93085db59d8b6f84c0a88e0415da01
Adrian <hannsadrian@gmail.com>
2020-03-18 13:27:31 +0100

    Add many providers

--- index.js
+++ index.js
@@ -1,37 +1,121 @@
 const createHafas = require("hafas-client");
 const createApi = require("hafas-rest-api");
 
-const bvgProfile = require("hafas-client/p/bvg");
-const dbProfile = require("hafas-client/p/db");
+const configs = [
+  {
+    hostname: "db.transportflow.online",
+    port: 3000,
+    provider: "DB (Deutsche Bahn)",
+    profile: require("hafas-client/p/db")
+  },
+  {
+    hostname: "bvg.transportflow.online",
+    port: 3001,
+    provider: "BVG (Berliner Verkehrsbetriebe)",
+    profile: require("hafas-client/p/bvg")
+  },
+  {
+    hostname: "vbb.transportflow.online",
+    port: 3002,
+    provider: "VBB (Verkehrsverbund Berlin-Brandenburg)",
+    profile: require("hafas-client/p/vbb")
+  },
+  {
+    hostname: "oebb.transportflow.online",
+    port: 3003,
+    provider: "ÖBB (Österreichische Bundesbahn)",
+    profile: require("hafas-client/p/oebb")
+  },
+  {
+    hostname: "insa.transportflow.online",
+    port: 3004,
+    provider: "NASA/INSA (Sachsen-Anhalt)",
+    profile: require("hafas-client/p/insa")
+  },
+  {
+    hostname: "nahsh.transportflow.online",
+    port: 3005,
+    provider: "NAHSH (Schleswig-Holstein)",
+    profile: require("hafas-client/p/nahsh")
+  },
+  {
+    hostname: "rmv.transportflow.online",
+    port: 3006,
+    provider: "RMV (Rhein-Main-Verkehrsverbund)",
+    profile: require("hafas-client/p/rmv")
+  },
+  {
+    hostname: "vgs.transportflow.online",
+    port: 3007,
+    provider: "Saarfahrplan/VGS",
+    profile: require("hafas-client/p/saarfahrplan")
+  },
+  {
+    hostname: "cfl.transportflow.online",
+    port: 3008,
+    provider: "CFL (Luxembourg National Railway Company)",
+    profile: require("hafas-client/p/cfl")
+  },
+  {
+    hostname: "hvv.transportflow.online",
+    port: 3009,
+    provider: "HVV (Hamburger Verkehrsverbund)",
+    profile: require("hafas-client/p/hvv")
+  },
+  {
+    hostname: "nvv.transportflow.online",
+    port: 3010,
+    provider: "NVV (Nordhessischer Verkehrsverbund)",
+    profile: require("hafas-client/p/nvv")
+  },
+  {
+    hostname: "vsn.transportflow.online",
+    port: 3011,
+    provider: "VSN (Verkehrsverbund Süd-Niedersachsen)",
+    profile: require("hafas-client/p/vsn")
+  },
+  {
+    hostname: "invg.transportflow.online",
+    port: 3012,
+    provider: "INVG (Ingolstädter Verkehrsgesellschaft)",
+    profile: require("hafas-client/p/invg")
+  },
+  {
+    hostname: "vbn.transportflow.online",
+    port: 3013,
+    provider: "VBN (Verkehrsverbund Bremen/Niedersachsen)",
+    profile: require("hafas-client/p/vbn")
+  },
+  {
+    hostname: "vmt.transportflow.online",
+    port: 3014,
+    provider: "VMT (Verkehrsverbund Mittelthüringen)",
+    profile: require("hafas-client/p/vmt")
+  }
+];
 
-const bvgConfig = {
-  hostname: "bvg.transportflow.de",
-  port: 3000,
-  name: "hafas-rest-endpoint",
-  homepage: "https://github.com/Adwirawien/HafasRestEndpoint",
-  version: "1.0.0",
-  description: "Transportflow hosted hafas-rest-api for Berliner Verkehrsbetriebe",
-  docsLink: " "
-};
-const dbConfig = {
-  hostname: "db.transportflow.de",
-  port: 3001,
-  name: "hafas-rest-endpoint",
-  homepage: "https://github.com/Adwirawien/HafasRestEndpoint",
-  version: "1.0.0",
-  description: "Transportflow hosted hafas-rest-api for Deutsche Bahn",
-  docsLink: " "
-};
+configs.forEach(data => {
+  const config = {
+    ...data,
+    name: "hafas-rest-endpoint",
+    homepage: "https://github.com/Transportflow/HafasRestEndpoint",
+    version: "1.0.0",
+    description: "Transportflow hosted api for " + data.provider,
+    docsLink: " "
+  };
 
-const bvgApi = createApi(createHafas(bvgProfile, "hafas-rest-endpoint"), bvgConfig, () => {});
-const dbApi = createApi(createHafas(dbProfile, "hafas-rest-endpoint"), dbConfig, () => {});
+  const api = createApi(
+    createHafas(config.profile, config.hostname),
+    config,
+    () => {}
+  );
 
-bvgApi.listen(bvgConfig.port, err => {
-  if (err) console.error(err);
-  console.log("BVG Api listening");
+  listen(api, config);
 });
 
-dbApi.listen(dbConfig.port, err => {
-  if (err) console.error(err);
-  console.log("DB Api listening");
-});
+async function listen(api, config) {
+  api.listen(config.port, err => {
+    if (err) console.error(err);
+    console.log(config.hostname + " listening");
+  });
+}
